2f42917dd64bd03fe6a208dc91aedf09e235cc1c
juliancito12 <126655566+juliancito12@users.noreply.github.com>
2023-03-08 16:23:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Añadir evento de click a cada botón de "Mostrar descripción"
 var toggleButtons = document.querySelectorAll(".toggle-description");
 toggleButtons.forEach(function(button) {
   button.addEventListener("click", function() {
-    // Cambiar el texto del botón y mostrar/ocultar la descripción correspondiente
     var description = button.nextElementSibling;
     if (description.classList.contains("hidden")) {
       description.classList.remove("hidden");
-      button.textContent = "Ocultar descripción";
+      button.textContent = "Leer menos";
     } else {
       description.classList.add("hidden");
-      button.textContent = "Mostrar descripción";
+      button.textContent = "Leer más";
     }
   });
 });
